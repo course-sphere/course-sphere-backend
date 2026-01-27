15196f6aeda1bc7e353278fb4c7a77100b31e8fd
akagiyuu <khanghuynh0245@gmail.com>
2026-01-27 22:06:17 +0700

    feat: init better auth

---
+++ services/auth-service/src/utils/auth.ts
@@ -0,0 +1,19 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "./db";
+import * as schema from "./db/auth-schema";
+
+export const auth = betterAuth({
+    emailAndPassword: {
+        enabled: true,
+    },
+    database: drizzleAdapter(db, {
+        provider: "pg",
+        schema: schema,
+    }),
+    trustedOrigins: [process.env.CORS_ORIGIN || "http://localhost:3001"],
+    logger: {
+        level: "debug",
+        log: (l, m, ...a) => console.log(`[better-auth][${l}]`, m, ...a),
+    },
+});
