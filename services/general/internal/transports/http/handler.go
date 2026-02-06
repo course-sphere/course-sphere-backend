37b182494d0bdad65eb04e7e232e35b75f3ce855
akagiyuu <khanghuynh0245@gmail.com>
2026-02-06 14:43:52 +0700

    feat(service/general): init token middleware

--- services/general/internal/transports/http/handler.go
+++ services/general/internal/transports/http/handler.go
@@ -3,9 +3,12 @@ package http
 import (
 	"net/http"
 
+	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/go-fuego/fuego"
+	"github.com/go-fuego/fuego/option"
 	httpSwagger "github.com/swaggo/http-swagger/v2"
 
+	"github.com/course-sphere/course-sphere-backend/pkg/middleware"
 	"github.com/course-sphere/course-sphere-backend/services/general/internal/usecase"
 )
 
@@ -20,7 +23,11 @@ func NewHandler(course *usecase.Course) *Handler {
 func (h *Handler) RegisterRoutes(s *fuego.Server) {
 	fuego.Get(s, "/", h.Ping)
 
-	course := fuego.Group(s, "/course")
+	// TODO: remove hard url
+	course := fuego.Group(s, "/course",
+		option.Middleware(middleware.MustToken("http://auth-service:3000")),
+		option.Security(openapi3.SecurityRequirement{"bearerAuth": []string{}}),
+	)
 	fuego.Get(course, "/{id}", h.GetCourse)
 }
 
