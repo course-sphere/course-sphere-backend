bb0c56dd633323726a8351aa2fc2f5b9276da353
akagiyuu <khanghuynh0245@gmail.com>
2026-02-17 18:09:55 +0700

    feat: enable verification and forgot password

--- services/auth/src/utils/auth.ts
+++ services/auth/src/utils/auth.ts
@@ -12,13 +12,32 @@ export const auth = betterAuth({
     baseURL: process.env.BETTER_AUTH_BASE_URL,
     emailAndPassword: {
         enabled: true,
-        sendResetPassword: async ({ user, url }, request) => {
+        requireEmailVerification: true,
+        sendResetPassword: async ({ user, token }, request) => {
+            const url = `${request?.headers.get("origin")}/auth/reset-password?token=${encodeURIComponent(token)}`;
+
             await transporter.sendMail({
                 to: user.email,
                 subject: "Reset your password",
                 html: await template(
                     "Reset Password",
                     "Click the button below to reset your password.",
+                    user,
+                    url,
+                    request,
+                ),
+            });
+        },
+    },
+    emailVerification: {
+        autoSignInAfterVerification: true,
+        sendVerificationEmail: async ({ user, url }, request) => {
+            await transporter.sendMail({
+                to: user.email,
+                subject: "Verify your email address",
+                html: await template(
+                    "Verify Email",
+                    "Click the button below to verify your email address.",
                     user,
                     url,
                     request,
