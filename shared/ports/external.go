a9108d7ee64bd2cc87d2878d44d4cc7adbdfa882
akagiyuu <khanghuynh0245@gmail.com>
2026-02-19 18:06:40 +0700

    refactor(shared): rename to auth-client

--- shared/ports/external.go
+++ shared/ports/external.go
@@ -3,9 +3,15 @@ package ports
 import (
 	"context"
 
+	"github.com/course-sphere/course-sphere-backend/shared/domain"
+	"github.com/google/uuid"
 	"github.com/lestrrat-go/jwx/v3/jwk"
 )
 
-type AuthService interface {
+type AuthClient interface {
 	MustGetJwks(ctx context.Context) jwk.Set
+}
+
+type UserClient interface {
+	Get(ctx context.Context, id uuid.UUID) (*domain.User, error)
 }
