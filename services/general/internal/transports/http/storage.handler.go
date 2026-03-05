b77c9cfa2edf160c1334009d62e70c29ddeae14a
akagiyuu <khanghuynh0245@gmail.com>
2026-03-05 18:01:34 +0700

    fix(s3): correct acl and content type setting

--- services/general/internal/transports/http/storage.handler.go
+++ services/general/internal/transports/http/storage.handler.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/go-fuego/fuego"
 	"github.com/jinzhu/copier"
 )
@@ -21,9 +22,12 @@ func (h *Handler) CreateS3PresignedURL(c fuego.ContextWithBody[CreatePresignedUR
 		Bucket:      aws.String(h.Config.S3Bucket),
 		Key:         aws.String(req.FileName),
 		ContentType: aws.String(req.ContentType),
-		ACL:         "public-read",
 	}, func(opt *s3.PresignPostOptions) {
 		opt.Expires = time.Minute * 10
+		opt.Conditions = append(opt.Conditions,
+			map[string]any{"acl": string(types.ObjectCannedACLPublicRead)},
+			map[string]any{"Content-Type": req.ContentType},
+		)
 	})
 	if err != nil {
 		return nil, fuego.BadRequestError{
@@ -31,6 +35,8 @@ func (h *Handler) CreateS3PresignedURL(c fuego.ContextWithBody[CreatePresignedUR
 			Detail: "Invalid file",
 		}
 	}
+	raw.Values["acl"] = string(types.ObjectCannedACLPublicRead)
+	raw.Values["Content-Type"] = req.ContentType
 
 	var resp PresignedURL
 	copier.Copy(&resp, raw)
