f0f2cc07ba012812a1b76786ffe09bad3c485ef4
akagiyuu <khanghuynh0245@gmail.com>
2026-03-07 23:11:42 +0700

    feat(general/ports): add get all course function

--- services/general/internal/ports/repo.go
+++ services/general/internal/ports/repo.go
@@ -11,5 +11,6 @@ import (
 type CourseRepository interface {
 	Create(ctx context.Context, instructorID uuid.UUID, data domain.CreateCourseData) (uuid.UUID, error)
 	Get(ctx context.Context, id uuid.UUID) (*domain.Course, error)
+	GetAll(ctx context.Context) ([]domain.Course, error)
 	Update(ctx context.Context, id uuid.UUID, instructorID uuid.UUID, data domain.UpdateCourseData) error
 }
