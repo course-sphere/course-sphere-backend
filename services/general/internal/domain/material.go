3094eb08874e3324b01d4d6925ffbf7579a4be20
akagiyuu <khanghuynh0245@gmail.com>
2026-03-08 13:52:51 +0700

    feat(general/domain/material): add Position

--- services/general/internal/domain/material.go
+++ services/general/internal/domain/material.go
@@ -14,37 +14,58 @@ const (
 
 type Material struct {
 	ID            uuid.UUID
+	Position      int64
 	Kind          MaterialKind
 	Lesson        string
 	Title         string
-	Content       string
+	Content       *string
 	RequiredScore *int64
 	RequiredPeers *int64
+	IsRequired    bool
+	Question      []Question
 }
 
 type CreateMaterialData struct {
 	Kind          MaterialKind
 	Lesson        string
 	Title         string
-	Content       string
+	Content       *string
 	RequiredScore *int64
 	RequiredPeers *int64
+	IsRequired    bool
 }
 
 type UpdateMaterialData struct {
+	Position      *int64
 	Lesson        *string
 	Title         *string
 	Content       *string
 	RequiredScore *int64
 	RequiredPeers *int64
-}
-
-type Question struct {
-	Question        string
-	PossibleAnswers []string
+	IsRequired    *bool
 }
 
 type QuestionCriterion struct {
 	Criterion string
 	Score     int64
+}
+
+type Question struct {
+	Question        string
+	Position        int64
+	PossibleAnswers []string
+	Criteria        []QuestionCriterion
+}
+
+type CreateQuestionData struct {
+	Question        string
+	PossibleAnswers []string
+	Criteria        []QuestionCriterion
+}
+
+type UpadteQuestionData struct {
+	Question        *string
+	Position        *int64
+	PossibleAnswers *[]string
+	Criteria        *[]QuestionCriterion
 }
