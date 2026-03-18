733bdda176654e8b72318d3ec28172d13cb7b15b
akagiyuu <khanghuynh0245@gmail.com>
2026-03-18 10:52:36 +0700

    feat(storage/usecase): add file permission check

--- services/storage/internal/usecase/storage.go
+++ services/storage/internal/usecase/storage.go
@@ -4,15 +4,23 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"slices"
+	"strings"
 
 	"github.com/google/uuid"
 
 	"github.com/course-sphere/course-sphere-backend/services/storage/internal/domain"
 	"github.com/course-sphere/course-sphere-backend/services/storage/internal/ports"
+	sharedPort "github.com/course-sphere/course-sphere-backend/shared/ports"
+)
+
+var (
+	UnauthorizedErr error = fmt.Errorf("User does not owned the course")
 )
 
 type Storage struct {
-	Inner ports.Storage
+	Inner         ports.Storage
+	GeneralClient sharedPort.GeneralClient
 }
 
 func (s *Storage) Upload(ctx context.Context, courseID uuid.UUID, data domain.UploadFileData) (string, error) {
@@ -36,6 +44,26 @@ func (s *Storage) Upload(ctx context.Context, courseID uuid.UUID, data domain.Up
 	return key, nil
 }
 
-func (s *Storage) Get(ctx context.Context, key string) (io.ReadCloser, error) {
+func (s *Storage) Get(ctx context.Context, token string, key string) (io.ReadCloser, error) {
+	parts := strings.SplitAfterN(key, "/", 2)
+	if len(parts) == 1 {
+		return s.Inner.Get(ctx, key)
+	}
+	if token == "" {
+		return nil, UnauthorizedErr
+	}
+
+	courseID, err := uuid.Parse(parts[0])
+	if err != nil {
+		return nil, err
+	}
+	enrolledCourses, err := s.GeneralClient.GetEnrolledCourses(ctx, token)
+	if err != nil {
+		return nil, err
+	}
+	if !slices.Contains(enrolledCourses, courseID) {
+		return nil, UnauthorizedErr
+	}
+
 	return s.Inner.Get(ctx, key)
 }
