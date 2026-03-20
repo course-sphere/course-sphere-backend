4aa6bd576ae531280c34d8f13e3dc13547215b18
akagiyuu <khanghuynh0245@gmail.com>
2026-03-20 15:14:48 +0700

    refactor(payment/repo): remove unused

--- services/payment/internal/adapters/repo/database.go
+++ services/payment/internal/adapters/repo/database.go
@@ -3,25 +3,20 @@ package repo
 import (
 	"context"
 
-	"github.com/course-sphere/course-sphere-backend/services/payment/internal/adapters/repo/database"
-	"github.com/course-sphere/course-sphere-backend/services/payment/internal/ports"
-	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/jinzhu/copier"
 	pgxUUID "github.com/vgarvardt/pgx-google-uuid/v5"
 )
 
 type Database struct {
-	pool *pgxpool.Pool
+	Wallet  WalletDatabase
+	History HistoryDatabase
 }
 
-var _ ports.Repository = &Database{}
-
-func NewDatabase(cfg *config.Config) (Database, error) {
-	pgxConfig, err := pgxpool.ParseConfig(cfg.DatabaseURL)
+func NewPool(databaseURL string) (*pgxpool.Pool, error) {
+	pgxConfig, err := pgxpool.ParseConfig(databaseURL)
 	if err != nil {
-		return Database{}, err
+		return nil, err
 	}
 
 	pgxConfig.AfterConnect = func(ctx context.Context, conn *pgx.Conn) error {
@@ -31,38 +26,21 @@ func NewDatabase(cfg *config.Config) (Database, error) {
 
 	pool, err := pgxpool.NewWithConfig(context.Background(), pgxConfig)
 	if err != nil {
-		return Database{}, nil
+		return nil, nil
 	}
 
-	return Database{pool}, nil
+	return pool, nil
 }
 
-func (db *Database) CreateWallet(ctx context.Context, userID uuid.UUID) error {
-	inner := database.New(db.pool)
+func NewDatabase(databaseURL string) (*Database, error) {
+	pool, err := NewPool(databaseURL)
 
-	return inner.CreateWallet(ctx, userID)
-}
+	if err != nil {
+		return nil, err
+	}
 
-func (db *Database) GetWallet(ctx context.Context, userID uuid.UUID) (int64, error) {
-	inner := database.New(db.pool)
-
-	return inner.GetWallet(ctx, userID)
-}
-
-func (db *Database) DepositWallet(ctx context.Context, userID uuid.UUID, amount int64) error {
-	inner := database.New(db.pool)
-
-	return inner.DepositWallet(ctx, database.DepositWalletParams{
-		UserID:        userID,
-		DepositAmount: amount,
-	})
-}
-
-func (db *Database) WithdrawWallet(ctx context.Context, userID uuid.UUID, amount int64) error {
-	inner := database.New(db.pool)
-
-	return inner.WithdrawWallet(ctx, database.WithdrawWalletParams{
-		UserID:         userID,
-		WithdrawAmount: amount,
-	})
+	return &Database{
+		Wallet:  WalletDatabase{Pool: pool},
+		History: HistoryDatabase{Pool: pool},
+	}, nil
 }
