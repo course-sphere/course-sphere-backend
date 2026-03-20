cf33e97316945003c6ae5f1d19b133b46fc23e2d
akagiyuu <khanghuynh0245@gmail.com>
2026-03-20 14:23:46 +0700

    feat(payment/ports): add wallet and history repo

--- services/payment/internal/ports/repo.go
+++ services/payment/internal/ports/repo.go
@@ -3,12 +3,17 @@ package ports
 import (
 	"context"
 
+	"github.com/course-sphere/course-sphere-backend/services/payment/internal/domain"
 	"github.com/google/uuid"
 )
 
-type Repository interface {
-	CreateWallet(ctx context.Context, userID uuid.UUID) error
-	GetWallet(ctx context.Context, userID uuid.UUID) (int64, error)
-	DepositWallet(ctx context.Context, userID uuid.UUID, amount int64) error
-	WithdrawWallet(ctx context.Context, userID uuid.UUID, amount int64) error
+type WalletRepository interface {
+	Create(ctx context.Context, userID uuid.UUID) error
+	GetByUser(ctx context.Context, userID uuid.UUID) (*domain.Wallet, error)
+	Update(ctx context.Context, id uuid.UUID, amount int64) error
+}
+
+type HistoryRepository interface {
+	Create(ctx context.Context, walletID uuid.UUID, data domain.CreateHistoryData) error
+	GetByWallet(ctx context.Context, walletID uuid.UUID) ([]domain.History, error)
 }
