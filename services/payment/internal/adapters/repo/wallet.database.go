d348d982e9d4ec6e669c7fd3815a4f13556938c3
akagiyuu <khanghuynh0245@gmail.com>
2026-03-20 14:44:00 +0700

    feat(payment/adapters/repo): implement wallet repo

---
+++ services/payment/internal/adapters/repo/wallet.database.go
@@ -0,0 +1,48 @@
+package repo
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jinzhu/copier"
+
+	"github.com/course-sphere/course-sphere-backend/services/payment/internal/adapters/repo/database"
+	"github.com/course-sphere/course-sphere-backend/services/payment/internal/domain"
+	"github.com/course-sphere/course-sphere-backend/services/payment/internal/ports"
+)
+
+type WalletDatabase struct {
+	Pool *pgxpool.Pool
+}
+
+var _ ports.WalletRepository = &CourseDatabase{}
+
+func (db *WalletDatabase) Create(ctx context.Context, userID uuid.UUID) error {
+	inner := database.New(db.Pool)
+
+	return inner.CreateWallet(ctx, userID)
+}
+
+func (db *WalletDatabase) GetByUser(ctx context.Context, userID uuid.UUID) (*domain.Wallet, error) {
+	inner := database.New(db.Pool)
+
+	raw, err := inner.GetWalletByUser(ctx, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	var wallet domain.Wallet
+	copier.Copy(&wallet, &raw)
+
+	return &wallet, nil
+}
+
+func (db *WalletDatabase) UpdateBalance(ctx context.Context, id uuid.UUID, amount int64) error {
+	inner := database.New(db.Pool)
+
+	return inner.UpdateWallet(ctx, database.UpdateWalletParams{
+		ID:     id,
+		Amount: amount,
+	})
+}
