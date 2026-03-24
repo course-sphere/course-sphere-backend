dd770c8e15b49f5d04ecb654235819c2924ae41d
akagiyuu <khanghuynh0245@gmail.com>
2026-03-24 15:43:36 +0700

    feat(general/database): add get attempt by id

--- services/general/internal/ports/repo.go
+++ services/general/internal/ports/repo.go
@@ -36,6 +36,7 @@ type AttemptRepository interface {
 	Create(ctx context.Context, materialID uuid.UUID, studentID uuid.UUID) (uuid.UUID, error)
 	CreateDetails(ctx context.Context, id uuid.UUID, data []domain.CreateAttemptDetailData) error
 	GetByMaterial(ctx context.Context, materialID uuid.UUID, studentID uuid.UUID) ([]domain.Attempt, error)
+	Get(ctx context.Context, id uuid.UUID) (*domain.Attempt, error)
 	GetDetails(ctx context.Context, id uuid.UUID) ([]domain.AttemptDetail, error)
 	Update(ctx context.Context, id uuid.UUID, score int32) error
 }
